1a6280480f36184a35a28785df67e54a949df11f
Ezequias Silva Santos <phd.esantos@gmail.com>
2024-07-13 19:25:33 -0400

    test: add initial tests for project

---
+++ tests/test_post.py
@@ -0,0 +1,11 @@
+import pytest
+
+from django.urls import reverse
+
+
+@pytest.mark.django_db
+
+def test_views(client):
+    url = reverse('index')
+    response =  client.get(url)
+    assert response.status_code == 200
